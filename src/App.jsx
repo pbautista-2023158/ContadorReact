6dfcc9fa7f2b122292ecd02c2f1ea25781026ad1
Informatica <informatica@alumno.local>
2025-04-03 17:18:37 -0600

    Ejercicio de React.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Home } from './components/Home'
 
 function App() {
   const persons = [
@@ -10,11 +11,40 @@ function App() {
     {name: 'Francisco', surname: 'Marroquín'}
   ]
 
+  //Constantes y variables
+  //useState = Datos variables... Que se pintan en pantalla
 
+  const num = 5
   const [count, setCount] = useState(0)
+  const countDisabled = count <= -100
+  const countDisabled2 = count >= 100
                                           //Valor inicial
   const [fullName, setFullName] = useState('Chupina despierto')
   const [sleep, setSleep] = useState(false)
+  const [arrayPersons, setArrayPersons] = useState(persons)
+  //Las funcionalidades se agregan antes del return
+
+  //Metodos y funciones
+  const addBautista = ()=>{
+    setArrayPersons([...persons, {name: 'Pedrito', surname: 'Bautista'}])
+    console.log(persons)
+  }
+
+  const sumar = ()=>{
+    setCount((prevCount)=> prevCount + num)
+  }
+
+  const restar = ()=>{
+    setCount((prevCount)=> prevCount - num)
+  }
+
+  //Hooks
+
+  if(num){
+   return <Home/>
+  }else{
+    
+  }
 
   return (
     <>
@@ -30,6 +60,36 @@ function App() {
         }
         }
       }>Actualizar nombre</button>
+
+      <button onClick={addBautista}>Agregar a Bautista</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* {} = Inyeccion de JS, solo puede llevar 1 instruccion*/}
+            <td>{arrayPersons[0].name}</td>
+            <td>{arrayPersons[0].surname}</td>
+          </tr>
+          <tr>
+            <td>{arrayPersons[1].name}</td>
+            <td>{arrayPersons[1].surname}</td>
+          </tr> 
+          <tr>
+            <td>{arrayPersons[2].name}</td>
+            <td>{arrayPersons[2].surname}</td>
+          </tr> 
+          <tr>
+            <td>{arrayPersons[3]?.name}</td> {/* Si tiene un ? es opcional, en este caso es el dato que le va a llegar del boton */}
+            <td>{arrayPersons[3]?.surname}</td>
+          </tr>                                   
+        </tbody>
+      </table>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -39,6 +99,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <button onClick={sumar} disabled={countDisabled2}>Sumar</button>
+      <h4>{count}</h4>
+      <button onClick={restar} disabled={countDisabled}>Restar</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
